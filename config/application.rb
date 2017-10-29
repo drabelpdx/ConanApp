0257b9b31e1f6d17a75936a60bc4bd8b83e8868b
drabelpdx <drabelpdx@gmail.com>
2017-10-29 12:07:16 -0700

    assests?

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module ConanApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.paths << Rails.root.join("app", "assets", "images")
+
   end
 end
