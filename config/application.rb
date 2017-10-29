7f71360761fc9ebb17ed80ccddec7dbf3286710b
drabelpdx <drabelpdx@gmail.com>
2017-10-29 12:53:50 -0700

    fix assets

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module ConanApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.paths << Rails.root.join("app", "assets", "images")
-    
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
@@ -23,8 +23,5 @@ module ConanApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    config.assets.paths << Rails.root.join("app", "assets", "images")
-
   end
 end
