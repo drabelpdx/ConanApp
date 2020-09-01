b56ca97dfadbb3268c2aa401ebc1abc0217304ba
David Abel <davidabel@MacBook-Pro.local>
2020-09-01 10:05:17 -0700

    removed sign in

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -8,19 +8,4 @@
 
   User.create!([
     { username: 'drabelpdx', email: 'drabelpdx@gmail.com', password: 'drabelpass', admin: true},
-    { username: 'salt1', email: 'salt1@example.com', password: 'salt1pass'},
-    { username: 'salt2', email: 'salt2@example.com', password: 'salt2pass'},
-    { username: 'salt3', email: 'salt3@example.com', password: 'salt3pass'},
-    { username: 'salt4', email: 'salt4@example.com', password: 'salt4pass'},
-    { username: 'salt5', email: 'salt5@example.com', password: 'salt5pass'},
-    { username: 'salt6', email: 'salt6@example.com', password: 'salt6pass'},
-    { username: 'salt7', email: 'salt7@example.com', password: 'salt7pass'},
-    { username: 'salt8', email: 'salt8@example.com', password: 'salt8pass'},
-    { username: 'salt9', email: 'salt9@example.com', password: 'salt9pass'},
-    { username: 'salt10', email: 'salt10@example.com', password: 'salt10pass'},
-    { username: 'salt11', email: 'salt11@example.com', password: 'salt11pass'},
-    { username: 'salt12', email: 'salt12@example.com', password: 'salt12pass'},
-    { username: 'salt13', email: 'salt13@example.com', password: 'salt13pass'},
-    { username: 'salt14', email: 'salt14@example.com', password: 'salt14pass'},
-    { username: 'salt15', email: 'salt14@example.com', password: 'salt15pass'}
   ])
